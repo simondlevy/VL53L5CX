ffdb37ccaf544ad60a8f3987d116a9c3b91f5fab
simondlevy <simon.d.levy@gmail.com>
2021-10-07 15:20:39 -0400

    More display

--- extras/display/display.py
+++ extras/display/display.py
@@ -13,9 +13,18 @@ import cv2
 import numpy as np
 from sys import stdout
 
+SCALEUP = 50
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SIZE = 0.5
+FONT_COLOR = (255,255,255)
+FONT_THICKNESS = 1
+
 def debug(s):
     print(s)
     stdout.flush()
+
+def new_image():
+    return np.zeros((8,8), dtype='uint8'), 0
 
 parser = argparse.ArgumentParser()
 
@@ -27,8 +36,7 @@ port = serial.Serial(args.port, 115200)
 
 count = 0
 
-image = np.zeros((8,8), dtype='uint8')
-count = 0
+image, count = new_image()
 
 while True:
 
@@ -36,15 +44,21 @@ while True:
 
     if b == 0xFF:
 
-        image = cv2.resize(image, (400,400), interpolation= cv2.INTER_NEAREST)
+        resized = cv2.resize(image, (8*SCALEUP,8*SCALEUP), interpolation= cv2.INTER_NEAREST)
 
-        cv2.imshow('VL53L5', image)
+        for j in range(8):
+            for k in range(8):
+                cv2.putText(resized,
+                            '%d' % image[k,j],
+                            (j*SCALEUP+SCALEUP//4,k*SCALEUP+SCALEUP//2),
+                            FONT, FONT_SIZE, FONT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+        cv2.imshow('VL53L5 [ESC to quit]', resized)
 
         if cv2.waitKey(1) == 27:
             break
 
-        image = np.zeros((8,8), dtype='uint8')
-        count = 0
+        image, count = new_image()
 
     else:
 
