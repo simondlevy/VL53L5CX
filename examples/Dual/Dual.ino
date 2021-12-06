d49b4e0ca7b336ba3ea4e80271ba0ae297fc13c2
simondlevy <simon.d.levy@gmail.com>
2021-12-05 21:34:58 -0500

    =More Dual work

--- examples/Dual/Dual.ino
+++ examples/Dual/Dual.ino
@@ -12,48 +12,11 @@
 #include <Debugger.hpp>
 #include "st/vl53l5cx_api.h"
 
-class MultiSupport {
 
-    private:
-
-        uint8_t _address;
-        uint8_t _int_pin;
-        uint8_t _lpn_pin;
-        VL53L5CX_Configuration _dev;
-        void (*_isr)(void);
-
-    public:
-
-        MultiSupport(uint8_t address, uint8_t int_pin, uint8_t lpn_pin, void (*isr)(void))
-        {
-            _address = address;
-            _int_pin = int_pin;
-            _lpn_pin = lpn_pin;
-            _isr = isr;
-        }
-}; 
-
+// Standard
 static const uint8_t LED_PIN = 13;
 
-// Suport for sensor 0 ------------------
-static const uint8_t INT_PIN_0 =  5;
-static const uint8_t LPN_PIN_0 =  9;
-static VL53L5CX_Configuration Dev_0;
-static volatile bool VL53L5_intFlag_0;
-static void isr0()
-{
-    VL53L5_intFlag_0 = true;
-} 
-
-// Suport for sensor 1 ------------------
-static const uint8_t INT_PIN_1 =  8;
-static const uint8_t LPN_PIN_1 =  4;
-static VL53L5CX_Configuration Dev_1;
-static volatile bool VL53L5_intFlag_1;
-static void isr1() 
-{
-    VL53L5_intFlag_1 = true;
-} 
+// Constants to fiddle with ------------------------
 
 // Min freq is 1 Hz max is 15 Hz (8 x 8) or 60 Hz (4 x 4) Sum of integration
 // time (1x for 4 x 4 and 4x for 8 x 8) must be 1 ms less than 1/freq,
@@ -62,142 +25,175 @@ static void isr1()
 // more noise in the ranging data
 static const uint8_t VL53L5_freq = 1;
 
-// time in milliseconds, settable only when in autonomous mode, otherwise a no op
+// Time in milliseconds, settable only when in autonomous mode, otherwise a no op
 static const uint8_t VL53L5_intTime = 10;
 
-static void enable(uint8_t pin)
-{
-    digitalWrite(pin, HIGH);
-}
+// --------------------------------------------------
 
-static void disable(uint8_t pin)
-{
-    digitalWrite(pin, LOW);
-}
+// Ad-hoc class for supporting multiple VL53L5cx sensors with interrupts
+class MultiSupport {
 
-static void setAddress(uint8_t pin, uint8_t address, VL53L5CX_Configuration * dev)
-{
-    enable(pin);   
-    vl53l5cx_set_i2c_address(dev, address<<1);
-    dev->platform.address = address;
-}
+    private:
 
-static void init(
-        uint8_t lpn_pin,
-        VL53L5CX_Configuration * dev,
-        uint8_t int_pin, 
-        void (*isr)(void))
-{
-    pinMode(lpn_pin, OUTPUT);
-    dev->platform.address = 0x29;
+        uint8_t _id; // for reportig
+        uint8_t _address;
+        uint8_t _int_pin;
+        uint8_t _lpn_pin;
+        VL53L5CX_Configuration _dev;
 
-    pinMode(int_pin, INPUT);
-    attachInterrupt(int_pin, isr, FALLING);
-}
+    public:
 
-static void start(uint8_t id, VL53L5CX_Configuration * dev)
-{
-    // Make sure there is a VL53L5CX_0 sensor connected
-    uint8_t isAlive = 0;
-    uint8_t error = vl53l5cx_is_alive(dev, &isAlive);
-    if (!isAlive || error) {
-        Debugger::reportForever("VL53L5CX_%d not detected at requested address", id);
-    }
+        // Set by interrupt service routine
+        volatile bool int_flag;
 
-    // Init VL53L5CX_0 sensor
-    error = vl53l5cx_init(dev);
-    if (error) {
-        Debugger::reportForever("VL53L5CX_%d ULD Loading failed", id);
-    }
-
-    Debugger::printf("VL53L5CX_%d ULD ready ! (Version : %s)\n",
-            VL53L5CX_API_REVISION, id);
-
-    error = vl53l5cx_set_resolution(dev, VL53L5CX_RESOLUTION_4X4);
-    if (error) {
-        Debugger::printf("vl53l5CX_set_resolution failed, error %u\n", error);
-    }
-
-    // set autonomous ranging mode //
-    error = vl53l5cx_set_ranging_mode(dev, VL53L5CX_RANGING_MODE_AUTONOMOUS);
-    if (error) {
-        Debugger::printf("VL53L5cX_set_ranging_mode failed, error %u\n", error);
-    }
-
-    // can set integration time in autonomous mode //
-    error = vl53l5cx_set_integration_time_ms(dev, VL53L5_intTime); //  
-    if (error) {
-        Debugger::printf("VL53L5CX_set_integration_time_ms failed, error %u\n", error);
-    }
-
-    // Select data rate //
-    error = vl53l5cx_set_ranging_frequency_hz(dev, VL53L5_freq);
-    if (error) {
-        Debugger::printf("VL53L5CX_set_ranging_frequency_hz failed, error %u\n", error);
-    }
-
-    // Set target order to closest //
-    error = vl53l5cx_set_target_order(dev, VL53L5CX_TARGET_ORDER_CLOSEST);
-    if (error) {
-        Debugger::printf("VL53L5cx_set_target_order failed, error %u\n", error);
-    }
-
-    // Get current integration time //
-    uint32_t integration_time_ms = 0;
-    error = vl53l5cx_get_integration_time_ms(dev, &integration_time_ms);
-    if (error) {
-        Debugger::printf("VL53L5cx_get_integration_time_ms failed, error %u\n", error);
-    }
-
-    Debugger::printf("Current integration time is : %d ms\n", integration_time_ms);
-
-    error = vl53l5cx_start_ranging(dev);
-    if (error !=0) {
-        Debugger::printf("start error = 0x%02X", error);
-    }
-
-    uint8_t isReady = 0;
-    vl53l5cx_check_data_ready(dev, &isReady); // clear the interrupt
-
-} // init
-
-
-static void checkAndReport(uint8_t id, volatile bool & flag, VL53L5CX_Configuration * dev)
-{
-    if (flag) {
-
-        flag = false;
-
-        uint8_t isReady = 0;
-
-        vl53l5cx_check_data_ready(dev, &isReady);
-
-        if (isReady) {
-
-            VL53L5CX_ResultsData results = {}; 
-
-            vl53l5cx_get_ranging_data(dev, &results);
-
-            // As the sensor is set in 4x4 mode by default, we have a total of
-            // 16 zones to print. For this example, only the data of first zone
-            // are printed 
-            Debugger::printf("VL53L5CX_%d data no : %3u\n", id, dev->streamcount);
-
-            for(uint8_t i=0; i<16; i++) {
-
-                Debugger::printf(
-                        "Zone : %3d, Status : %3u, Distance : %4d mm\n",
-                        i,
-                        results.target_status[VL53L5CX_NB_TARGET_PER_ZONE*i],
-                        results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*i]);
-            }
-            Debugger::printf("\n");
+        MultiSupport(uint8_t id, uint8_t address, uint8_t int_pin, uint8_t lpn_pin)
+        {
+            _id = id;
+            _address = address;
+            _int_pin = int_pin;
+            _lpn_pin = lpn_pin;
+            int_flag = false;
         }
-    }
-}
 
-static MultiSupport sensor0 = MultiSupport(0x27, 5, 9, isr0);
-static MultiSupport sensor1 = MultiSupport(0x29, 8, 4, isr1);
+        void init(void (*isr)(void))
+        {
+            pinMode(_lpn_pin, OUTPUT);
+            _dev.platform.address = 0x29;
+
+            pinMode(_int_pin, INPUT);
+            attachInterrupt(_int_pin, isr, FALLING);
+        }
+
+        void start(void)
+        {
+            // Make sure there is a VL53L5CX_0 sensor connected
+            uint8_t isAlive = 0;
+            uint8_t error = vl53l5cx_is_alive(&_dev, &isAlive);
+            if (!isAlive || error) {
+                Debugger::reportForever("VL53L5CX not detected at requested address");
+            }
+
+            // Init VL53L5CX_0 sensor
+            error = vl53l5cx_init(&_dev);
+            if (error) {
+                Debugger::reportForever("VL53L5CX_%d ULD Loading failed");
+            }
+
+            Debugger::printf("VL53L5CX_ULD ready ! (Version : %s)\n",
+                    VL53L5CX_API_REVISION);
+
+            error = vl53l5cx_set_resolution(&_dev, VL53L5CX_RESOLUTION_4X4);
+            if (error) {
+                Debugger::printf("vl53l5CX_set_resolution failed, error %u\n", error);
+            }
+
+            // set autonomous ranging mode //
+            error = vl53l5cx_set_ranging_mode(&_dev, VL53L5CX_RANGING_MODE_AUTONOMOUS);
+            if (error) {
+                Debugger::printf("VL53L5cX_set_ranging_mode failed, error %u\n", error);
+            }
+
+            // can set integration time in autonomous mode //
+            error = vl53l5cx_set_integration_time_ms(&_dev, VL53L5_intTime); //  
+            if (error) {
+                Debugger::printf("VL53L5CX_set_integration_time_ms failed, error %u\n", error);
+            }
+
+            // Select data rate //
+            error = vl53l5cx_set_ranging_frequency_hz(&_dev, VL53L5_freq);
+            if (error) {
+                Debugger::printf("VL53L5CX_set_ranging_frequency_hz failed, error %u\n", error);
+            }
+
+            // Set target order to closest //
+            error = vl53l5cx_set_target_order(&_dev, VL53L5CX_TARGET_ORDER_CLOSEST);
+            if (error) {
+                Debugger::printf("VL53L5cx_set_target_order failed, error %u\n", error);
+            }
+
+            // Get current integration time //
+            uint32_t integration_time_ms = 0;
+            error = vl53l5cx_get_integration_time_ms(&_dev, &integration_time_ms);
+            if (error) {
+                Debugger::printf("VL53L5cx_get_integration_time_ms failed, error %u\n", error);
+            }
+
+            Debugger::printf("Current integration time is : %d ms\n", integration_time_ms);
+
+            error = vl53l5cx_start_ranging(&_dev);
+            if (error !=0) {
+                Debugger::printf("start error = 0x%02X", error);
+            }
+
+            uint8_t isReady = 0;
+            vl53l5cx_check_data_ready(&_dev, &isReady); // clear the interrupt
+
+        } // run
+
+        void enable(void)
+        {
+            digitalWrite(_lpn_pin, HIGH);
+        }
+
+        void disable(void)
+        {
+            digitalWrite(_lpn_pin, LOW);
+        }
+
+        void setAddress(uint8_t address)
+        {
+            vl53l5cx_set_i2c_address(&_dev, address<<1);
+            _dev.platform.address = address;
+        }
+
+        void checkAndReport(void)
+        {
+            if (int_flag) {
+
+                int_flag = false;
+
+                uint8_t isReady = 0;
+
+                vl53l5cx_check_data_ready(&_dev, &isReady);
+
+                if (isReady) {
+
+                    VL53L5CX_ResultsData results = {}; 
+
+                    vl53l5cx_get_ranging_data(&_dev, &results);
+
+                    // As the sensor is set in 4x4 mode by default, we have a total of
+                    // 16 zones to print. For this example, only the data of first zone
+                    // are printed 
+                    Debugger::printf("VL53L5CX_%d data no : %3u\n", _id, _dev.streamcount);
+
+                    for(uint8_t i=0; i<16; i++) {
+
+                        Debugger::printf(
+                                "Zone : %3d, Status : %3u, Distance : %4d mm\n",
+                                i,
+                                results.target_status[VL53L5CX_NB_TARGET_PER_ZONE*i],
+                                results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*i]);
+                    }
+                    Debugger::printf("\n");
+                }
+            }
+        }
+
+}; 
+
+static MultiSupport sensor0 = MultiSupport(0, 0x27, 5, 9);
+static MultiSupport sensor1 = MultiSupport(1, 0x29, 8, 4);
+
+static void isr0()
+{
+    sensor0.int_flag = true;
+} 
+
+static void isr1() 
+{
+    sensor1.int_flag = true;
+} 
 
 void setup(void)
 {
@@ -210,19 +206,20 @@ void setup(void)
     Wire.setClock(400000);           // Set I2C frequency at 400 kHz  
     delay(1000);
 
-    init(LPN_PIN_0, &Dev_0, INT_PIN_0, isr0);
-    init(LPN_PIN_1, &Dev_1, INT_PIN_1, isr1);
+    sensor0.init(isr0);
+    sensor1.init(isr1);
 
-    disable(LPN_PIN_0);  // disable VL53L5CX_0
+    sensor0.disable();
+    sensor1.enable();
 
-    setAddress(LPN_PIN_1, 0x27, &Dev_1);
+    sensor1.setAddress(0x27);
 
-    enable(LPN_PIN_0);   // enable VL53L5CX_0
+    sensor0.enable();
 
     delay(100);
 
-    start(0, &Dev_0);
-    start(1, &Dev_1);
+    sensor0.start();
+    sensor1.start();
 
     digitalWrite(LED_PIN, LOW); // turn off led when initiation successful
 
@@ -230,8 +227,8 @@ void setup(void)
 
 void loop(void)
 {
-    checkAndReport(0, VL53L5_intFlag_0, &Dev_0);
-    checkAndReport(1, VL53L5_intFlag_1, &Dev_1);
+    sensor0.checkAndReport();
+    sensor1.checkAndReport();
 
     // Blinkety blink!
     digitalWrite(LED_PIN, HIGH);
