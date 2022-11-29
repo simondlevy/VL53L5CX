387858f4502829dc48e1fc3a8e3648471f802d95
simondlevy <simon.d.levy@gmail.com>
2022-11-29 17:26:28 -0500

    Fixed constructor call in Resolution sketch

--- examples/Resolution/Resolution.ino
+++ examples/Resolution/Resolution.ino
@@ -9,13 +9,13 @@
 #include <Wire.h>
 #include "VL53L5cx.h"
 
+static const uint8_t LPN_PIN = 5;
+
 // Choices are RESOLUTION_4X4 and RESOLUTION_8x8
 static VL53L5cx::resolution_t RESOLUTION = VL53L5cx::RESOLUTION_8X8;
 //static VL53L5cx::resolution_t RESOLUTION = VL53L5cx::RESOLUTION_4X4;
 
-static VL53L5cx sensor = VL53L5cx(5, // LPN pin
-                                  0x29, // I^2C address
-                                  RESOLUTION); 
+static VL53L5cx sensor = VL53L5cx(LPN_PIN, RESOLUTION); 
 
 // Determine image size from resolution
 static const uint8_t SIZE = RESOLUTION == VL53L5cx::RESOLUTION_8X8 ? 8 : 4;
