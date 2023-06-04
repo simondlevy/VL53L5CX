562f17b6919a4679255e4d7e2a48f6af32e7ae69
simondlevy <simon.d.levy@gmail.com>
2023-06-04 05:14:09 +0000

    Start wiringpi example

---
+++ wiringpi/basic.cpp
@@ -0,0 +1,14 @@
+/* 
+   basic.cpp: Basic VL53L5 example using Raspbery Pi WiringPi library
+
+   Copyright (C) 2023 Simon D. Levy
+
+   MIT license
+*/
+
+#include <wiringPi.h>
+
+int main(int argc, char ** argv)
+{
+    return 0;
+}
