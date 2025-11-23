5967d6760722f1bc929c4a1d39fbc223cf52490c
simondlevy <simon.d.levy@gmail.com>
2025-11-23 18:01:48 -0500

    Cleanup

--- examples/Display/display.py
+++ examples/Display/display.py
@@ -17,15 +17,18 @@ from sys import stdout
 SCALEUP = 50
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 FONT_SIZE = 0.5
-FONT_COLOR = (255,255,255)
+FONT_COLOR = (255, 255, 255)
 FONT_THICKNESS = 1
+
 
 def debug(s):
     print(s)
     stdout.flush()
 
+
 def new_image():
-    return np.zeros((8,8), dtype='uint8'), 0
+    return np.zeros((8, 8), dtype='uint8'), 0
+
 
 parser = argparse.ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
@@ -45,16 +48,18 @@ while True:
 
     if b == 0xFF:
 
-        resized = cv2.resize(image, (8*SCALEUP,8*SCALEUP), interpolation= cv2.INTER_NEAREST)
+        resized = cv2.resize(image, (8*SCALEUP, 8*SCALEUP),
+                             interpolation=cv2.INTER_NEAREST)
 
         for j in range(8):
             for k in range(8):
                 cv2.putText(resized,
-                            '%d' % image[k,j],
-                            (j*SCALEUP+SCALEUP//4,k*SCALEUP+SCALEUP//2),
-                            FONT, FONT_SIZE, FONT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+                            '%d' % image[k, j],
+                            (j*SCALEUP+SCALEUP//4, k*SCALEUP+SCALEUP//2),
+                            FONT, FONT_SIZE, FONT_COLOR, FONT_THICKNESS,
+                            cv2.LINE_AA)
 
-        cv2.imshow(      'VL53L5 [ESC to quit]', resized)
+        cv2.imshow('VL53L5 [ESC to quit]', resized)
 
         if cv2.waitKey(1) == 27:
             break
@@ -63,6 +68,5 @@ while True:
 
     else:
 
-        image[8-(count//8)-1, 8-(count%8)-1] = b
+        image[8-(count // 8)-1, 8-(count % 8)-1] = b
         count += 1
-
